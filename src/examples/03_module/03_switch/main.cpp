b1f80b18e7c908dc723851b49ab025fdc4774d6e
alejandro1-dot <alejandro.garcia-vazquez@g.austincc.edu>
2021-02-11 20:04:34 -0600

    Switch changes

--- src/examples/03_module/03_switch/main.cpp
+++ src/examples/03_module/03_switch/main.cpp
@@ -1,8 +1,15 @@
 #include<iostream>
-
+#include "switch.h"
+using std::cin; using std::cout;
 int main() 
 {
+	auto option = o;
+	cou<<"Enter option {1-4}: ";
+	cin>>option 
+
+	auto result = menu(option);
 	
+	cout<<result;
 
 	return 0;
 }
