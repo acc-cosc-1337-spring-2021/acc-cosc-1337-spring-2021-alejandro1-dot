12618427286956f114b549a79c5889473f57e8c2
alejandro1-dot <alejandro.garcia-vazquez@g.austincc.edu>
2021-02-11 21:23:59 -0600

    Homework 1: Receot

--- test/homework_test/01_variables_test/01_variables_tests.cpp
+++ test/homework_test/01_variables_test/01_variables_tests.cpp
@@ -12,3 +12,15 @@ TEST_CASE("Verify sum_numbers function")
 	REQUIRE(add_numbers(10, 10) == 20);
 }
 
+
+TEST_CASE("Verify Meal amount of tax", "verification")
+{
+	REQUIRE(get_sales_tax(10) == .675);
+	REQUIRE(get_sales_tax(20) == 1.35);
+} 
+
+TEST_CASE("Verify tip amount in relation to meal cost", "verification")
+{
+	REQUIRE(get_tips(20, 15) == 3);
+	REQUIRE(get_tips(20, 20) == 4);
+}
