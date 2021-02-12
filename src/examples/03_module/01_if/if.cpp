46d6a650a3b6f082a98135a93485d7dbf0614937
alejandro1-dot <alejandro.garcia-vazquez@g.austincc.edu>
2021-02-11 20:08:06 -0600

    IF -> Classwork_Changes

--- src/examples/03_module/01_if/if.cpp
+++ src/examples/03_module/01_if/if.cpp
@@ -9,3 +9,25 @@
 //hours over 40, false otherwise
 
 
+
+bool is_in_range_1_to_10(int num)
+{
+    return num >= 1 && num <= 10;
+}
+
+bool is_not_in_range_1_to_10(int num) //and
+{
+    return (!(num >= 1 && num <= 10));
+}
+
+// TEsting Numbers less than 1 and greater than 10
+
+bool is_not_in_range_1_to_10_OR(int num);
+{
+    return num < 1 || num > 10;
+}
+
+bool is_char_a_consonant(char c)
+{
+    return (!(c = 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'));
+}
