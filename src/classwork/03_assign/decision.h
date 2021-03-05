1eaff44b15b108906c614f19cda7884833652418
alejandro1-dot <alejandro.garcia-vazquez@g.austincc.edu>
2021-03-04 20:15:58 -0600

    Homework 3 submission

--- src/classwork/03_assign/decision.h
+++ src/classwork/03_assign/decision.h
@@ -1,2 +1,9 @@
+#include <string>
+
+
 //header
 bool is_even(int testNumber);
+//Getting letter grade from if statement 
+std::string get_letter_grade_using_if(int grade);
+//Getting Letter grade using Switch statement
+std::string get_letter_grade_using_switch(int grade);
--- test/classwork_test/03_assign_test/decisions_tests.cpp
+++ test/classwork_test/03_assign_test/decisions_tests.cpp
@@ -1,8 +1,33 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "loops.h"
+#include "decision.h"
+
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
+
+
+TEST_CASE("Test get_letter_grade_using_if", "verification") 
+{
+	REQUIRE(get_letter_grade_using_if(80) == "B");
+	REQUIRE(get_letter_grade_using_if(93) == "A");
+	REQUIRE(get_letter_grade_using_if(79) == "C");
+	REQUIRE(get_letter_grade_using_if(63) == "D");
+	REQUIRE(get_letter_grade_using_if(49) == "F");
+	REQUIRE(get_letter_grade_using_if(-12) == "Number entered cannot give Letter Grade");
+}
+
+TEST_CASE("Test get_letter_grade_using_switch", "verification") 
+{
+	REQUIRE(get_letter_grade_using_switch(80)== "B");
+	REQUIRE(get_letter_grade_using_switch(100)== "A");
+	REQUIRE(get_letter_grade_using_switch(77)== "C");
+	REQUIRE(get_letter_grade_using_switch(63)== "D");
+	REQUIRE(get_letter_grade_using_switch(30)== "F");
+	REQUIRE(get_letter_grade_using_switch(-3)== "Number entered cannot give Letter Grade");
+}
+
+
 
