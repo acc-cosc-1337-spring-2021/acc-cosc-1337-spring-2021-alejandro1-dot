8ab357965f4da0dc49baa891a4d2094801942aaa
alejandro1-dot <alejandro.garcia-vazquez@g.austincc.edu>
2021-03-09 16:22:19 -0600

    Module 1, numbers

--- src/examples/01_module/04_decimals/numbers.h
+++ src/examples/01_module/04_decimals/numbers.h
@@ -3,7 +3,7 @@
 Write double value-return function named add_to_double_1 that accepts a double parameter
 */
 
-
+int add_to_Double(double number);
 /*
 Write double value-return function named add_to_double_2 that accepts a double parameter
 */
