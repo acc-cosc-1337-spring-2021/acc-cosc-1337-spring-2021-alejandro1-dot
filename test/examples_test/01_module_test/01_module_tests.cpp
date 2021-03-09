30d5fa0302d635d0691dd0b66c397da075741453
alejandro1-dot <alejandro.garcia-vazquez@g.austincc.edu>
2021-03-09 02:11:43 -0600

    Module 1, example 2,

--- test/examples_test/01_module_test/01_module_tests.cpp
+++ test/examples_test/01_module_test/01_module_tests.cpp
@@ -1,13 +1,29 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
-#include "numbers.h"
+//Module 03
+#include "input.h"
+//Module 02
+#include "vars.h"
 #include<string>
 
-TEST_CASE("Verify Test Configuration", "verification") {
+
+TEST_CASE("Verify Test Configuration", "verification") 
+{
 	REQUIRE(true == true);
 }
 
 /*test case echo_variable */
+
+TEST_CASE("Verify echo variable function", "returns a single digit")
+{
+	REQUIRE(echo_variable(5) == 5);
+}
+/*test case get_total with 3 and 3 as parameter*/
+
+TEST_CASE("Verify echo variable function", "returns a single digit")
+{
+	REQUIRE(get_total(5,5) == 5);
+}
 
 /*test case add_to_double_1 with 0 as parameter*/
 
