ee3dabce41e9a9696a70bea71e5f765ffbd38cf2
alejandro1-dot <alejandro.garcia-vazquez@g.austincc.edu>
2021-03-09 19:34:42 -0600

    Module 1, strings

--- src/examples/01_module/07_string/main.cpp
+++ src/examples/01_module/07_string/main.cpp
@@ -1,8 +1,20 @@
+//TO use string we must first declare the include<string>
+#include<string>
+#include<iostream>
 
+//Then narrow it doew further
+
+using std::string; using std::cout;
 
 int main() 
 {
 	//Create string variable and display it		
+	string name = "john"; //is a class
 
+	//name.append
+
+	//char name2 = 'a' // is not a class
+	cout<<"your name is: "<<name;
+	
 	return 0;
 }
