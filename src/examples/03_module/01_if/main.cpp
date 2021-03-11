42a3dcfffe7def7abcffacc1ec4dd3e49991f1c3
alejandro1-dot <alejandro.garcia-vazquez@g.austincc.edu>
2021-03-11 01:37:00 -0600

    Module 3,if

--- src/examples/03_module/01_if/main.cpp
+++ src/examples/03_module/01_if/main.cpp
@@ -7,14 +7,20 @@ using std::cin;
 int main() 
 {
 	//create a bool variable named even
+	bool even;
 	//create an int variable named value
-
+	int value;
 	//prompt user for a keyboard number
+	cout<<"enter a number to see if its even: ";
 	//assign the number to the value variable
-
+	cin>> value;
 	//call the is_even function with value as its argument
+	even = is_even(even);
 	//and assign its return value to the even variable
-	
+	if (even)
+	{
+		cout<<" is even"
+	}
 
 	//display even if number is even
 
