69907a32d1b8063ffdc9e7093e03ce7714140bef
alejandro1-dot <alejandro.garcia-vazquez@g.austincc.edu>
2021-04-21 00:36:18 -0500

    Vectors

--- src/examples/04_module/05_value_and_reference_params/value_ref.h
+++ src/examples/04_module/05_value_and_reference_params/value_ref.h
@@ -1,3 +1,10 @@
+#include "for_ranged.h"
+#include<iostream>
+#include<vector>
+
+using std::vector;
+
+using std::cout;
 
 /*
 Write function prototype for pass_by_val_and_ref with
@@ -10,3 +17,5 @@ void pass_by_val_and_ref(int value, int& num2);
 Write function prototype for pass_by_const_ref with
 const int reference parameter
 */
+
+void pass_by_const_ref(const int& value)
--- src/examples/04_module/06_vectors/main.cpp
+++ src/examples/04_module/06_vectors/main.cpp
@@ -1,7 +1,26 @@
 #include "vec.h"
+#include<string>
+using std::vector;
+using std::cout; 
+using std::string;
 
 int main() 
 {
+	vector<int> nums; 
+	nums.push_back(10);
+	nums.push_back(2);
+	nums.push_back(4);
+
+	loop_vector_w_index_val(nums);
+
+	vector<string> names{"joe", "mary", "john"};
+
+	names[0]="jerry";
+
+	for(auto name: names)
+	{
+		cout<<name<<"\n";
+	}
 
 	return 0;
 }
